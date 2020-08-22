4bac115d62253dbc63a06f50331b34c0c6f17aa5
Linus Feldt <linus@Linuss-MBP.lan>
2020-08-22 20:50:17 +0200

    456

--- pUI.lua
+++ pUI.lua
@@ -122,31 +122,7 @@ hooksecurefunc("PlayerFrame_UpdateStatus", function()
        PlayerAttackGlow:Hide()
        PlayerStatusGlow:Hide()
     end
- end)
-
- -- Castbar timer
-CastingBarFrame.timer = CastingBarFrame:CreateFontString(nil)
-CastingBarFrame.timer:SetFont("Fonts\\ARIALN.ttf", 16, "THINOUTLINE")
-CastingBarFrame.timer:SetPoint("RIGHT", CastingBarFrame, "CENTER", 0, -25)
-CastingBarFrame.update = 0.1
-     
-local function CastingBarFrame_OnUpdate_Hook(self, elapsed)
-	if not self.timer then return end
-	if self.update and self.update < elapsed then
-		if self.casting then
-			self.timer:SetText(format("%.1f", max(self.maxValue - self.value, 0)))
-		elseif self.channeling then
-			self.timer:SetText(format("%.1f", max(self.value, 0)))
-		else
-			self.timer:SetText("")
-		end
-		self.update = .1
-	else
-		self.update = self.update - elapsed
-	end
-end
-
-CastingBarFrame:HookScript('OnUpdate', CastingBarFrame_OnUpdate_Hook)
+end)
 
 --[[ Unitframes ]]
 --TotemFrame:ClearAllPoints() 
